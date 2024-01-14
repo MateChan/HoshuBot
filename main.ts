8813df13998a001c4f9aa1b4203c1b1dc6ee4e65
MateChan <rohichi.0826@gmail.com>
2024-01-14 16:11:30 +0900

    first commit

---
+++ main.ts
@@ -0,0 +1,13 @@
+import { Webhook } from "https://deno.land/x/discord_webhook@1.0.0/mod.ts";
+
+const webhook = new Webhook(Deno.env.get("DISCORD_WEBHOOK_URL") ?? "");
+
+Deno.cron("hoshu", "* * * * *", () => {
+  const border = 1 / (7 * 24 * 60);
+  const rand = Math.random();
+  console.log({ border, rand });
+  if (rand < border) {
+    console.log("保守");
+    webhook.post("保守");
+  }
+});
