9332fe71d327ddfcc8d739f2b4ca171c44dca774
MateChan <rohichi.0826@gmail.com>
2024-01-14 16:15:35 +0900

    保守しやすくした

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import { Webhook } from "https://deno.land/x/discord_webhook@1.0.0/mod.ts";
 const webhook = new Webhook(Deno.env.get("DISCORD_WEBHOOK_URL") ?? "");
 
 Deno.cron("hoshu", "* * * * *", () => {
-  const border = 1 / (7 * 24 * 60);
+  const border = 1 / (5 * 24 * 60);
   const rand = Math.random();
   console.log({ border, rand });
   if (rand < border) {
